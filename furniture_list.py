38ce37501c8210648b2805856d5b0694c4f582e6
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:34:15 +0800

    furniture_list

---
+++ furniture_list.py
@@ -0,0 +1,17 @@
+#1
+furnitures_list = ["Stool", "Figurines", "Wall table", "Bed", "Closet", "Dresser", "Bookshelf", "Nightstand", "Chair", "Futon", "Relaxing Chair", "Table"]
+
+#2 
+print(furnitures_list)
+
+#3
+print(furnitures_list[8])
+
+#4
+furnitures_list[5] = "Sofa"
+
+#5
+del furnitures_list[10]
+
+#6
+print("The last index is ", furnitures_list[-1])
