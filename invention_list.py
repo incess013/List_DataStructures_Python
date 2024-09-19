c9a835cabcebaa9a46a4e767f8d925f1cf7dff73
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:24:32 +0800

    invention_list

---
+++ invention_list.py
@@ -0,0 +1,17 @@
+#1
+invention_list = ["Vulcanization","Dynamite","Ballpoint pen","Mobilephone","Television","Tire","Photography","Brake"]
+
+#2
+print(invention_list)
+
+#3
+print(invention_list[6])
+
+#4
+invention_list[2] = "Lightbulb"
+
+#5
+del invention_list[5]
+
+#6
+print("The last index is ", invention_list[-1])
