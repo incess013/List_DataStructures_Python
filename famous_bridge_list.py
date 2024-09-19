8ba63b322598722823f071c2560fe8c91a8a98a6
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:33:56 +0800

    famous_bridge_list

---
+++ famous_bridge_list.py
@@ -0,0 +1,18 @@
+#1
+famous_bridge_list = ["Charles Bridge", "Milennium Bridge", "Akashi Kaikyō Bridge", "Brooklyn Bridge", "Tower Bridge", "Verrazano-Narrows", "Rio Niterói Bridge", 
+"Pont Neuf",  "Sydney Harbour Bridge", "Pont du Gard",]
+
+#2
+print(famous_bridge_list)
+
+#3
+print(famous_bridge_list[3])
+
+#4
+famous_bridge_list[6] = "Golden Gate Bridge" 
+
+#5
+del famous_bridge_list[7]
+
+#6
+print("The last index is ", famous_bridge_list[-1])
