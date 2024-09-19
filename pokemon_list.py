1c01ddeb7e3451603ddb83b93f615ebe19d737ef
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:21:06 +0800

    pokemon_list

---
+++ pokemon_list.py
@@ -0,0 +1,20 @@
+#1
+pokemon_list = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill"]
+
+#2
+print(pokemon_list)
+
+#3 
+print(pokemon_list[9])
+
+#4 
+pokemon_list[12] = "Pikachu"
+
+#5
+del pokemon_list[10]
+
+#6
+print(pokemon_list[4:8])
+
+#7
+print("The last index is ",pokemon_list[-1])
