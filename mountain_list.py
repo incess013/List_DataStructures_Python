3d86650e1810de53dd29a28bd241b469d4b7c2e8
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:17:18 +0800

    mountain_list

---
+++ mountain_list.py
@@ -0,0 +1,17 @@
+#1
+mountain_list = ["Everest","Kangchenjunga","Lhotse","Makalu","Cho Oyu","Dhaulagiri","Manaslu","Nanga Parbat"]
+
+#2
+print(mountain_list)
+
+#3
+print(mountain_list[5])
+
+#4 
+mountain_list[3] = "Everest"
+
+#5
+del mountain_list[6]
+
+#6
+print("The index is ", mountain_list[-1])
