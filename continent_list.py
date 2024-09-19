7499273e2e6aecc9b48cb793dae4223809b1093b
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:30:17 +0800

    continent_list

---
+++ continent_list.py
@@ -0,0 +1,20 @@
+#1
+continent_list = ["Asia", "Africa", "North America", "South America", "Antarctica", "Europe", "Australia"]
+
+#2 
+print(continent_list)
+
+#3 
+print(continent_list[4])
+
+#4
+continent_list[2] = "Africa"
+
+#5
+del continent_list[4]
+
+#6
+print("The Last index is ", continent_list[-1])
+
+
+
