23e3a6ef3111665f337faaf72850da7b17f34d98
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:07:05 +0800

    game_list

---
+++ game_list.py
@@ -0,0 +1,20 @@
+#1
+game_list = ["PUBG", "Sims", "Call of Duty", "League of Legends", "Valorant", "Dota", "Roblox", "Counter", "Bowmasters", "Piano Tiles", "Roll the Ball", "Clash Royale", "Candy Crush", "Chess", "Fortnite"]
+
+#2
+print(game_list)
+
+#3 
+print(game_list[7])
+
+#4
+game_list[4] = "Minecraft"
+
+#5
+del game_list[9]
+
+#6
+print(game_list[5:11])
+
+#7
+print("The last index is ", game_list[-1])
