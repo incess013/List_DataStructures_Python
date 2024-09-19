3e107f7e43fa334bdca3561a026a3b4cee53b4a6
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:01:00 +0800

    fruit_list

---
+++ fruit_list.py
@@ -0,0 +1,21 @@
+#1
+fruit_list = ["Apricot","Grapes","Cherry","Damson","Dates","Apple","Nectarine","Peach","Plum","Lemon","Banana","Watermelon"]
+
+#2
+print(fruit_list)
+
+#3
+print(fruit_list[9])
+
+#4
+fruit_list[2] = "Mango"
+
+#5
+del fruit_list[10]
+
+#6
+print(fruit_list[4:8])
+
+#7
+print("last index is ", fruit_list[-1])
+
