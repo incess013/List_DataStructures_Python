3531f6885d70bdd549d119200bb0fbe36ecb0d79
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:27:19 +0800

    continent_country_list

---
+++ continent_country_list.py
@@ -0,0 +1,20 @@
+#1
+continent_country_list = ["India - Asia", "Japan - Asia", "Canada - North America", "South Africa - Africa", "Argentina - South America", "Germany - Europe", "Brazil - South America", "China - Asia", "Australia - Australia", "United States - North America", "France - Europe", "Nigeria - Africa", "Mexico - North America", "Egypt - Africa", "New Zealand - Australia"]
+
+#2
+print(continent_country_list)
+
+#3
+print(continent_country_list[9])
+
+#4
+continent_country_list[10] = "Australia"
+
+#5
+del continent_country_list[12]
+
+#6
+print(continent_country_list[4:9])
+
+#7
+print("The last index is ", continent_country_list[-1])
