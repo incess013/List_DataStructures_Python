a53affd1f1a47f91f30a3ca816934b7f91575ac3
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:19:22 +0800

    currency_list

---
+++ currency_list.py
@@ -0,0 +1,17 @@
+#1
+currency_list = ["AUD", "CAD", "USD", "PHP", "NZD", "GBP", "JPY" "CNY", "HKD", "TRY"]
+
+#2
+print(currency_list)
+
+#3
+print(currency_list[4])
+
+#4
+currency_list[7] = "Euro"
+
+#5
+del currency_list[9]
+
+#6
+print("The last index is ", currency_list[-1])
