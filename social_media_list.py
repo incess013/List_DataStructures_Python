e5660586e5d6c2a476ef1e7e84f4e6e9c9695f21
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:35:36 +0800

    social_media_list

---
+++ social_media_list.py
@@ -0,0 +1,17 @@
+#1
+social_media_list = ["YouTube", "Telegram", "Facebook", "Whatsapp", "Twitter", "Threads", "Linkedln", "WeChat", "Discord", "Tumblr"]
+
+#2
+print(social_media_list)
+
+#3
+print(social_media_list[5])
+
+#4
+social_media_list[3] = "Instagram"
+
+#5
+del social_media_list[8]
+
+#6
+print("The last index is ", social_media_list[-1])
