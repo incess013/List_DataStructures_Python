24b3f784c69fe3b28b1dfdd9b75f23818656df8a
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:02:06 +0800

    building_list

---
+++ building_list.py
@@ -0,0 +1,17 @@
+#1
+building_list = ["Machu Picchu", "Seoul Tower", "Pyramid of Giza", "Tower of Pisa", "Sagrada Familia", "Petra", "Fallingwater", "Chichen Itza"]
+
+#2
+print(building_list)
+
+#3
+print(building_list[5])
+
+#4
+building_list[2] = "Eiffel Tower"
+
+#5
+del building_list[6]
+
+#6
+print("the last index is ", building_list[-1])
