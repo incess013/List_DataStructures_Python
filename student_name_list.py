f25ebee990b4337dd9f5622efee645c0d91d5137
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 10:44:52 +0800

    student_name_list

---
+++ student_name_list.py
@@ -0,0 +1,23 @@
+#1
+student_name_list = ["Shamelle","Ram","Andrey","Kisseraskley","Carol","Shamel","Emmanuel","Charles","Jinelyn","Mj","Mat","Sandara","Jhoncell","Christian","Elijah"]
+
+#a
+print(student_name_list)
+
+#b
+print("15th is", student_name_list[14])
+
+#c
+student_name_list[11] = "John"
+
+#d
+del student_name_list[9]
+
+#d2
+print(student_name_list)
+
+#e
+print(student_name_list[2:6])
+
+#f
+print("Last index is", student_name_list[-1])
