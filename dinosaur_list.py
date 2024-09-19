6878fbbba3ff1ae3c85d4521b54d77d0ce93211c
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:28:20 +0800

    dinosaur_list

---
+++ dinosaur_list.py
@@ -0,0 +1,17 @@
+#1
+dinosaur_list = ["Achillobator", "Acrocanthosaurus", "Alamosaurus", "Albertaceratops", "Allosaurus", "Amargasaurus", "Anchiceratops", "Abrictosaurus"]
+
+#2 
+print(dinosaur_list)
+
+#3
+print(dinosaur_list[4])
+
+#4
+dinosaur_list[6] = "Tyrannousaurus Rex"
+
+#5
+del dinosaur_list[7]
+
+#6
+print("The last index is ",dinosaur_list[-1])
