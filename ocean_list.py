dacaa78a5c3e605c106efe06df456618f870bd00
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:17:14 +0800

    ocean_list

---
+++ ocean_list.py
@@ -0,0 +1,17 @@
+#1
+oceans_list = ["Arctic", "North Atlantic", "South Atlantic", "North Pacific", "Southern oceans"] 
+
+#2
+print(oceans_list)
+
+#3
+print(oceans_list[3])
+
+#4
+oceans_list[2] = "Pacific Ocean"
+
+#5
+del oceans_list[4]
+
+#6
+print("The last index is ", oceans_list[-1])		
