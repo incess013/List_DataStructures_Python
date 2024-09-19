b29be660b227b813f107e5cf93b1e1afb25e8e4d
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:16:16 +0800

    job_titles_list

---
+++ job_titles_list.py
@@ -0,0 +1,17 @@
+#1
+job_title_list = ["Aerospace Engineer", "Data entry clerk", "Office Manager", "Project manager", "Administrative Assistant", "System Administrator", "Financial Analyst", "Hotel Manager", "Marketing Coordinator", "Nurse"]
+
+#2 
+print(job_title_list)
+
+#3 
+print(job_title_list[6])
+
+#4
+job_title_list[5] = "Software Engineer" 
+
+#5
+del job_title_list[9]
+
+#6
+print("The last index is ", job_title_list[-1])
