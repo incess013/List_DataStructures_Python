4a6a2d7deaaea39b6759da7cb9c6638a7122fac7
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:04:55 +0800

    school_subject_list

---
+++ school_subject_list.py
@@ -0,0 +1,20 @@
+#1
+school_subject = ["History", "Geography", "English",  "Science", "Social Studies", "Philosophy", "Health Education", "Economics", "Engineering", "Psychology", "Music", "PE"]
+
+#2
+print(school_subject)
+
+#3
+print(school_subject[6])
+
+#4
+school_subject[8] = "Mathematics" 
+
+#5
+del school_subject[4]
+
+#6
+print(school_subject[2:6])
+
+#7
+print("The last index is ", school_subject[-1])
