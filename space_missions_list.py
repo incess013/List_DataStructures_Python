1b94b2a0f4a9fa628251c262709eb31483d71f1d
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:36:13 +0800

    space

---
+++ space_missions_list.py
@@ -0,0 +1,17 @@
+#1
+space_missions_list = ["Mir", "Voyager 1", "Voyager 2", "Sputnik", "Vostok", "ISS", "Preserverance Rover", "Apollo 13", "New Horizons", "Curiosity Rover", "ISS"]
+
+#2
+print(space_missions_list)
+
+#3
+print(space_missions_list[7])
+
+#4 
+space_missions_list[4] = "Apollo 11"
+
+#5
+del space_missions_list[6]
+
+#6
+print("The last index is ", space_missions_list[-1])
