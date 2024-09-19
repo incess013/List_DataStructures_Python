2fa73b516196c7124e6ee174f61e2bd3cdd56175
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:36:36 +0800

    sports_list

---
+++ sports_list.py
@@ -0,0 +1,17 @@
+#1
+sports_list = ["Volleyball", "Tennis", "Soccer", "Baseball", "Hockey", "Golf", "Cricket", "Badminton", "Football", "Rugby"]
+
+#2
+print(sports_list)
+
+#3
+print(sports_list[6])
+
+#4
+sports_list[4] = "Basketball"
+
+#5
+del sports_list[9]
+
+#6
+print("The last index is ", sports_list[-1])
