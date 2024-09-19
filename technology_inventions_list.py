972f3142e0fc732b3248d5417d7cf557c123e70d
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:11:02 +0800

    technology_inventiond_list

---
+++ technology_inventions_list.py
@@ -0,0 +1,20 @@
+#1
+technology_inventions_list = ["Self Driving Cars", "Voice Assitants", "Biotechnology", "3D Printing", "Blockchain", "5G Technology", "AI", "IoT", "AR", "VR", "Drones", "Smart home devices", "AR Glasses", "Gene Editing", "AR Glasses"]
+
+#2 
+print(technology_inventions_list)
+
+#3
+print(technology_inventions_list[11]) 
+
+#4
+technology_inventions_list[8] = "Smartphone"
+
+#5
+del technology_inventions_list[14]
+
+#6
+print(technology_inventions_list[5:11])
+
+#7
+print("The last index is ", technology_inventions_list[-1])
