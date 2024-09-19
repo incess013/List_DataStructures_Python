08fe386e11857f41f244019a834b4b1ecfd7f7f8
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:03:11 +0800

    vegetable_list

---
+++ vegetable_list.py
@@ -0,0 +1,17 @@
+#1
+vegetable_list = ["Turnip","Tomato","Okra","Butternut","Squash","Acorn","Pumpkin"]
+
+#2
+print(vegetable_list)
+
+#3 
+print(vegetable_list[5])
+
+#4 
+vegetable_list[3] = "Spinach."
+
+#5 
+del vegetable_list[6]
+
+#6
+print("the last index is ", vegetable_list[-1])
