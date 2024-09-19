39785d1fecb0eef3a76b8a35cafab76293b85794
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:04:55 +0800

    movie_list

---
+++ movie_list.py
@@ -0,0 +1,20 @@
+#1
+movie_list = ["Nashville","Sunrise","Amadeus","Jaws”,"Casablanca","Airplane","Freaks","Goodfellas","Heat","Detour","Shrek","Rushmore","Ran","Magnolia","Oldboy","Gilda","Brazil","Psycho","Frankenstein","Rebecca"]
+
+#2
+print(movie_list)
+
+#3
+print(movie_list[12])
+
+#4
+movie_list[15] = "Inception"
+
+#5
+del movie_list[18]
+
+#6
+print(movie_list[8:14])
+
+#7
+print("the last index is ", movie_list[-1])
