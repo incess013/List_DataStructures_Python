ff819b7f61ae5b6143a58cdcb6777594837ebccd
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:18:18 +0800

    bird_species_list

---
+++ bird_species_list.py
@@ -0,0 +1,20 @@
+#1
+bird_species_list = ["Hawk-Eagle", "Cockatoo", "Eagle-Owl", "Tawny breated", "Black-naped Oriole", "Kakapo",  "Whiskered Tern", "Barn Swallow", "Yellow Breasted Dove", "Gallicolumba Crinigera"]
+
+#2
+print(bird_species_list)
+
+#3
+print(bird_species_list[9])
+
+#4
+bird_species_list[6] = "Eagle"
+
+#5
+del bird_species_list[7]
+
+#6
+print(bird_species_list[3:8])
+
+#7
+print("The last index is ", bird_species_list[-1])
