94936c6c75fa91097cefb95c5593e13b1d5a85b7
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:15:13 +0800

    animal_habitats_list

---
+++ animal_habitats_list.py
@@ -0,0 +1,17 @@
+#1
+animal_habitats_list = ["Temperate Forest", "Freshwater", "Ocean", "Rainforest", "Forest", "Desert", "Polar", "Mountain"] 
+
+#2
+print(animal_habitats_list)
+
+#3
+print(animal_habitats_list[5])
+
+#4 
+animal_habitats_list[3] = "Savannah"
+
+#5
+del animal_habitats_list[6]
+
+#6
+print("The last index is ", animal_habitats_list[-1])
