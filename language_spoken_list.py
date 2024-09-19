2b77d04cce2e3056fde5000987f3c9838c39d76f
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:16:24 +0800

    language_spoken_list

---
+++ language_spoken_list.py
@@ -0,0 +1,20 @@
+#1
+language_spoken_list = ["Bengali", "Portuguese", "Russian", "English", "Tagalog", "Chinese", "Hindi", "Japanese", "Yue Chinese", "Vietnamese", "Turkish", "Wu Chinese", "Marathi", "Korean", "Italian", "Southern Min", "French", "Javanese" "Hausa", "Tamil"] 
+
+#2 
+print(language_spoken_list)
+
+#3 
+print(language_spoken_list[13])
+
+#4
+language_spoken_list[10] = "Spanish"
+
+#5
+del language_spoken_list[16]
+
+#6
+print(language_spoken_list[6:12])
+
+#7
+print("The last index is ", language_spoken_list[-1])
