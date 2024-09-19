3c39befa12ee1004278625c55a577980e2ec37d4
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:23:29 +0800

    'historical_figures_list

---
+++ historical_figures_list.py
@@ -0,0 +1,17 @@
+#1
+historical_figures = [ "Aristotle", "Karl Marx", "Christopher Columbus", "Alexander the Great", "Willian Shakespeare", "Adolf Hitler", "Abraham Lincoln", "Henry VIII", "Julius Caesar", "Thomas Jefferson"]
+
+#2
+print(historical_figures)
+
+#3
+print(historical_figures[8])
+
+#4
+historical_figures[4] = "Nelson Mandela"
+
+#5
+del historical_figures[7]
+
+#6
+print("The last index is ", historical_figures[-1])
