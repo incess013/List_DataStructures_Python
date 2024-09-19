6e77be005b9784ce6106a61a710a85940ab3d084
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:26:10 +0800

    holiday_list

---
+++ holiday_list.py
@@ -0,0 +1,20 @@
+#1
+holiday_list = ["Labor day", "Easter Day", "All saints Day", "Black Saturday","Independence Day", "Valentines", "Thanksgiving Day", "Rizal Day", "Father's Day","New Year", "Ninoy Aquino Day", "Good Friday"]
+
+#2
+print(holiday_list)
+
+#3
+print(holiday_list[9])
+
+#4
+holiday_list[3] = "Christmas"
+
+#5
+del holiday_list[11]
+
+#6
+print(holiday_list[2:8])
+
+#7
+print("The last index is ", holiday_list[-1])
