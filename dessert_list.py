160bdff24a2b82ea129dda3db0c1d8ac6c8b5a49
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:27:27 +0800

    dessert_list'

---
+++ dessert_list.py
@@ -0,0 +1,17 @@
+#1
+dessert_list = ["Chocolate Frappe", "Banana Pudding", "S'Mores", "Tiramisu cake", "Apple Cinnamon", "Ice Cream", "Chocolates", "Almond Cookies", "Pies"]
+
+#2
+print(dessert_list)
+
+#3
+print(dessert_list[4])
+
+#4
+dessert_list[6] = "Chocolate Cake"
+
+#5
+del dessert_list[3]
+
+#6
+print("The last index is ", dessert_list[-1])
