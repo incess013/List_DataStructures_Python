c49943babcde3875831af7c06ba4419364f09212
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:31:08 +0800

    river_list

---
+++ river_list.py
@@ -0,0 +1,17 @@
+#1
+river_list = ["Amazon River", "Danube River", "Mississippi River", "Yangtze River", "Ganges", "River Thames", "Mekong River", "Congo River", "Volga River", "Orinoco"]
+
+#2
+print(river_list)
+
+#3
+print(river_list[6])
+
+#4
+river_list[8] = "Nile River"
+
+#5
+del river_list[9]
+
+#6
+print("The last index is " ,river_list[-1])
