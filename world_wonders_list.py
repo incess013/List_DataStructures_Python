4397162a611f46f9fb670ca8e213cbdde067bdd2
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:35:23 +0800

    world_wonders_list

---
+++ world_wonders_list.py
@@ -0,0 +1,17 @@
+#1
+world_wonders = ["Great Wall of China", "Chichén Itzá", "Petra", "Machu Picchu", "Christ the Redeemer", "Colosseum", "Taj Mahal"]
+
+#2
+print(world_wonders)
+
+#3
+print(world_wonders[4])
+
+#4
+world_wonders[2] = "Great Wall of China"
+
+#5
+del world_wonders[5]
+
+#6
+print("The last index is ", world_wonders[-1])
