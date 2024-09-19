ec1099ffa5c72386ad8d48984f9a3fd09ea5c91c
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:23:04 +0800

    recipe_list

---
+++ recipe_list.py
@@ -0,0 +1,20 @@
+#1
+recipe_list = ["Scrambled Eggs", "French Toast", "Bacon", "Vinaigrette", "Grilled Cheese", "Chicken Soup", "Roasted Chicken", "Stir-fry vegetable", "Spaghetti", "Chicken Curry", "Tacos", "Salmon", "Chocolate cookies", "Apple pie", "Sushi"]
+
+#2 
+print(recipe_list)
+
+#3
+print(recipe_list[12])
+
+#4
+recipe_list[9] = "Lasagna"
+
+#5
+del recipe_list[11]
+
+#6
+print(recipe_list[5:11])
+
+#7
+print("The last index is ", recipe_list[-1])
