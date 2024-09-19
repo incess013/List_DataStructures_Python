e5b74e443c1f5dd426d162d1902c702097046f41
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:29:12 +0800

    natural_landmark_list

---
+++ natural_landmark_list.py
@@ -0,0 +1,18 @@
+#1
+natural_landmark_list = ["Victoria Falls", "Hạ Long Bay", "Uyuni Salt Flat", "Uluru", "Iguazu Falls", "Cliffs of Moher", "Great Barrier Reef", "Puerto Princesa Subterranean River", "National Park", "Milford Sound", "Angel Falls"]
+
+#2
+print(natural_landmark_list)
+
+#3
+print(natural_landmark_list[8])
+
+#4
+natural_landmark_list[5] = "Grand Canyon"
+
+#5
+del natural_landmark_list[9]
+
+#6
+print("The last index is ", natural_landmark_list[-1])
+
