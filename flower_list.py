a44f3386c17c949b36f42446cf9ffe8d6d6304dc
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:11:34 +0800

    flower_list

---
+++ flower_list.py
@@ -0,0 +1,17 @@
+#1
+flower_list = ["Zinnia", "Aster", "Begonia", "Lotus", "Magnolia", "Plumeria", "Acacia", "Daisy", "Daffodil", "Hibiscus"]
+
+#2 
+print(flower_list)
+
+#3
+print(flower_list[5])
+
+#4
+flower_list[8] = "Rose"
+
+#5
+del flower_list[2]
+
+#6
+print("The last index in ", flower_list[-1])
