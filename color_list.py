50b308f5605bd59f9abb64a195eb08d513a2a945
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 10:59:26 +0800

    color_list

---
+++ color_list.py
@@ -0,0 +1,20 @@
+#1
+color_list = ["Purple","Silver","Yellow","Red","Green","White","Pink","Gray","Black","Blue","Black","White","Cyan","Brown","Gold"]
+
+#2
+print(color_list)
+
+#3 
+print(color_list[10])
+
+#4
+color_list[4] = "Purple"
+
+#5 
+del color_list[5]
+
+#6
+print(color_list[2:9])
+
+#7
+print("last index is", color_list[-1])
