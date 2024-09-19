38f5d1eb3aa83525a2e850991485a224beb9f331
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:08:59 +0800

    planet_list

---
+++ planet_list.py
@@ -0,0 +1,17 @@
+#1
+planet_list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+#2
+print(planet_list)
+
+#3
+print(planet_list[3])
+
+#4
+planet_list[7] = "Pluto"
+
+#5
+del planet_list[4]
+
+#6
+print("The last index is ", planet_list[-1])
