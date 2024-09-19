d49efb3f91622cffd5b1d0201df899c0c6de9735
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:28:43 +0800

    festival_list

---
+++ festival_list.py
@@ -0,0 +1,20 @@
+#1
+festival_list = ["Electric Daisy Carnival", "Essence", "Sziget", "Coachella", "Rock In Rio", "Woodstock", "Summerfest", "Mawazine", "Donauinselfest", "Chinese New Year", "Carnival of Venice", "Tomorrowland ", "Gion Matsuri", "San Fermín", "Moriones Festival"]
+
+#2
+print(festival_list)
+
+#3
+print(festival_list[7])
+
+#4
+festival_list[11] = "Diwali"
+
+#5
+del festival_list[9]
+
+#6
+print(festival_list[5:13])
+
+#7
+print("The last index is ", festival_list[-1])
