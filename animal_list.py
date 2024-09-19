ade13259f82d14147aeb082564fa73dec4ce2654
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 10:48:18 +0800

    animal_list

---
+++ animal_list.py
@@ -0,0 +1,18 @@
+#1
+animal_list = ["Dog","Cat","Horse","Donkey","Tiger","Lion","Cheetah","Bear","Elephant","Turtle","Rabbit"]
+
+#2
+print(animal_list)
+
+#3
+print(animal_list[3])
+
+#4
+animal_list[6] = "Elephant" 
+
+#5
+del animal_list[8]
+
+#6
+print("last index is", animal_list[-1])
+
