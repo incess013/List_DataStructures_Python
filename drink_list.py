257d75affb885c07b8fc2a47f04a1702eb091759
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:24:07 +0800

    drink_list

---
+++ drink_list.py
@@ -0,0 +1,17 @@
+#1
+drink_list = ["Margarita", "Cosmopolitan", "Daiquiri", "Gimlet", "Manhattan", "Negroni", "Old Fashioned", "Mimosa"]
+
+#2
+print(drink_list)
+
+#3
+print(drink_list[4])
+
+#4
+drink_list[3] = "Coffee"
+
+#5
+del drink_list[7]
+
+#6
+print("The latest index is ", drink_list[-1])
