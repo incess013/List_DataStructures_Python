4106e55e450774b46ea168f92a7b98e70cfa4322
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:22:23 +0800

    animal_species_list

---
+++ animal_species_list.py
@@ -0,0 +1,17 @@
+#1
+animal_species = ["Mammals", "Birds", "Reptiles", "Amphibians", "Fishes", "Insects", "Arachnids"]
+
+#2
+print(animal_species)
+
+#3
+print(animal_species[2])
+
+#4
+animal_species[5] = "Gorilla"
+
+#5
+del animal_species[6]
+
+#6
+print("The last index is ", animal_species[-1])
