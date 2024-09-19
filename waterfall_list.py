d1bab5f661cca3268eaf3eb359dcf710d0b2ce6b
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:32:59 +0800

    waterfall_list

---
+++ waterfall_list.py
@@ -0,0 +1,17 @@
+#1
+waterfalls_list = ["Niagara Falls", "Victoria Falls", "Gullfoss Falls", "Angel Falls", "Iguazu Falls", "Sutherland Falls", "Yosemite Falls", "Ban Gioc Waterfalls", "Jog Falls", "Kaieteur Falls"]
+
+#2
+print(waterfalls_list)
+
+#3
+print(waterfalls_list[8])
+
+#4
+waterfalls_list[5] = "Niagara Falls" 
+
+#5
+del waterfalls_list[9]
+
+#6
+print("The last index is ", waterfalls_list[-1])
