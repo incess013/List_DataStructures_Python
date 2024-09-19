8ef99904a4e2523554a6876a59b3f233ddf9818a
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:25:10 +0800

    university_list

---
+++ university_list.py
@@ -0,0 +1,17 @@
+#1
+university_list = ["University of Oxford","University of Cambridge","Harvard University","UCL","Massachusetts Institute of Technology","Stanford University","University of Pennsylvania","California Institute of Technology", "Cornell University", "Yale University"]
+
+#2
+print(university_list)
+
+#3
+print(university_list[6])
+
+#4
+university_list[4] = "Harvard University"
+
+#5
+del university_list[9]
+
+#6
+print("The last index is ", university_list[-1])
