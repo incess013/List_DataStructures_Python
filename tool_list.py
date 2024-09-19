ebe3cc4516b7dd9b268436683f5651c9ece4737e
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:09:55 +0800

    tool_list

---
+++ tool_list.py
@@ -0,0 +1,17 @@
+#1
+tool_list = ["Level", "Drill", "Jigsaw", "Screwdriver", "Tape Measure", "Saw", " Chisel", "Pliers", "Sander", "Router"] 
+
+#2
+print(tool_list)
+
+#3
+print(tool_list[4])
+
+#4
+tool_list[7] = "Hammer" 
+
+#5
+del tool_list[5]
+
+#6
+print("The last index is ", tool_list[-1])
