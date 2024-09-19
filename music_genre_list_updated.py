1f775730cf21b36f25c675a6f2cf407964e4b527
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:50:55 +0800

    music_genre_list_updated

---
+++ music_genre_list_updated.py
@@ -0,0 +1,20 @@
+#1
+music_genre_list = ["Pop music", "Classical music", "Jazz", "Hip hop music", "Heavy metal", "Folk music", "Alternative rock", "Funk", "World music", "Reggae", "Punk", "Rock", "Indie rock", "Soul music", "Disco"]
+
+#2
+print(music_genre_list)
+
+#3
+print(music_genre_list[10])
+
+#4
+music_genre_list[5] = "Jazz"
+
+#5
+del music_genre_list[7]
+
+#6
+print(music_genre_list[3:9])
+
+#7
+print("The last index is ", music_genre_list[-1])	
