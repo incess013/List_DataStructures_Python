41b617e274b644814bdf66f6f00324b82ddb4e6f
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:30:15 +0800

    land_animal_speed_list

---
+++ land_animal_speed_list.py
@@ -0,0 +1,20 @@
+#1
+land_animal_speed = ["Giraffe - 56km/hr", "Horse - 88km/hr", "Gray Wolf - 64km/hr", "Elephant - 40km/hr", "Lion - 80km/hr", "Hippopotamus - 48km/hr", "Pronghorn Antelope - 88km/hr", "Gazelle - 80km/hr", "Rhinoceros - 56km/hr", "Kangaroo - 48km/hr", "Zebra - 64km/hr", "American Bison - 56km/hr"]
+
+#2
+print(land_animal_speed)
+
+#3
+print(land_animal_speed[7])
+
+#4
+land_animal_speed[9] = "Cheetah - 120km/hr"
+
+#5
+del land_animal_speed[10]
+
+#6
+print(land_animal_speed[3:8])
+
+#7
+print("The last index is ", land_animal_speed[-1]) 
