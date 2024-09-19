e38186d8b07d899a7819f5f768918eb6f492eb4d
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:15:07 +0800

    author_list

---
+++ author_list.py
@@ -0,0 +1,20 @@
+#1
+author_list= ["Louisa May Alcott", "Samuel Clemens", "Charles Dickens", "Clive Staple Lewis ", "Leo Tolstoy", "Herman Melville", "Agatha Christine", "Mary Ann Evans", "John Bunyan", "Lewis", "Alexandre Dumas", "Jane Austen"] 
+
+#2
+print(author_list)
+
+#3
+print(author_list[6])
+
+#4
+author_list[4] = "Mark Twain"
+
+#5
+del author_list[10]
+
+#6
+print(author_list[3:9])
+
+#7
+print("the last index is ", author_list[-1])
