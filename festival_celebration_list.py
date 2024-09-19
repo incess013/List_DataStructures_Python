6f13269c11921a1374cbe952b2cc1a45e296e46d
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:26:14 +0800

    festival_celebration_list

---
+++ festival_celebration_list.py
@@ -0,0 +1,17 @@
+#1
+festival_celebration_list = ["Kadayawan", "Sinulog", "Pahiyas", "Panagbenga", "Diwali", "Mardi Gras", "Chinese New year", "Ati-atihan", "Day of the dead", "Oktoberfest" ]
+
+#2
+print(festival_celebration_list)
+
+#3
+print(festival_celebration_list[5])
+
+#4
+festival_celebration_list[3] = "Thanksgiving"
+
+#5
+del festival_celebration_list[7]
+
+#6
+print("The last index is " , festival_celebration_list[-1])
