2b50834599fcad63501f177570fd045193947cba
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:03:06 +0800

    actor_list

---
+++ actor_list.py
@@ -0,0 +1,17 @@
+#1
+actor_list = ["Robert Downey Jr.", "Scarlett Johansson", "Mark Ruffalo", "Chris Pratt", "Don Cheadle", "Chris Hemsworth", "Samuel L. Jackson", "Chris Evans", "Sebastian Stan", "Tom Hiddleston"]
+
+#2 
+print(actor_list)
+
+#3
+print(actor_list[7])
+
+#4
+actor_list[3] = "Leonardo DiCarpio"
+
+#5
+del actor_list[8]
+
+#6
+print("The last index is ", actor_list[-1]) 
