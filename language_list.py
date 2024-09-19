bc0b9a73168ee348c700dbebe2dec3d6de1c1282
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:10:09 +0800

    language_list

---
+++ language_list.py
@@ -0,0 +1,20 @@
+#1
+language_list = ["SQL","TypeScript","Pascal","Ruby","Dart","Kotlin","NoSQL","C#","C++","Java","JavaScript","R","Perl","Swift"]
+
+#2
+print(language_list)
+
+#3
+print(language_list[11])
+
+#4 
+language_list[9] = "Python"
+
+#5 
+del language_list[13]
+
+#6
+print(language_list[5:13])
+
+#7
+print("The last index is ", language_list[-1])
