2ff9166e8418b66ae5b68a03258a23d4d851b350
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 12:31:59 +0800

    volcano_list

---
+++ volcano_list.py
@@ -0,0 +1,17 @@
+#1
+volcano_list = ["Etna", "Kīlauea", "Merapi", "Mount Nyiragongo", "Sakurajima", "Mauna Kea", "Mayon"]
+
+#2
+print(volcano_list)
+
+#3
+print(volcano_list[4])
+
+#4
+volcano_list[3] = "Mount Vesuvius"
+
+#5
+del volcano_list[5]
+
+#6
+print("The last index is ", volcano_list[-1])
