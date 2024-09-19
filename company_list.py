06dbf4098580c0f1c284c1936dfed55e59a4fa66
Princess Rodiel <cssjhnnamae@gmail.com>
2024-09-19 11:18:29 +0800

    company_list

---
+++ company_list.py
@@ -0,0 +1,17 @@
+#1
+company_list = ["Apple", "Microsoft", "NVIDIA", "McDonald's", "Amazon", "Saudi Aramco", "Meta", "Berkshire Hathaway", "Coca-Cola", "Mastercard"]
+
+#2
+print(company_list)
+
+#3
+print(company_list[7])
+
+#4
+company_list[5] = "Apple"
+
+#5
+del company_list[8]
+
+#6
+print("the last index is ", company_list[-1])
